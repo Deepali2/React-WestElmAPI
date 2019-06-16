3c69671e82aa1c4718150d34f7815d7c48c289b5
Deepali2 <hdgarg@gmail.com>
2019-06-15 21:25:05 -0700

    adds the Arrow and the ImageSlide Components from the non-modular app already built by me under the name of React-WestElm-Webpage-API

---
+++ src/components/Carousel.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+import ImageSlide from './ImageSlide';
+import Arrow from './Arrow';
+
+class Carousel extends React.Component {
+  constructor (props) {
+  super(props);		
+  this.state = {
+  currentImageIndex: 0
+  };
+
+  this.nextSlide = this.nextSlide.bind(this);
+  this.previousSlide = this.previousSlide.bind(this);
+}
+
+  previousSlide () {
+    const lastIndex = this.props.imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+    
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide () {
+    const lastIndex = this.props.imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  render () {
+    return (          
+      <div className="carousel">
+        <Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
+        <ImageSlide url={ this.props.imgUrls[this.state.currentImageIndex] } />
+        <Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
+      </div>
+    );
+  }
+}
+
+export default Carousel;
+
