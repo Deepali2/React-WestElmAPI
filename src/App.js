c92114c72d3f630960418df66f26523cb9a0a0c4
Deepali2 <hdgarg@gmail.com>
2019-06-16 15:28:06 -0700

    adds the page heading and styles it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css';
 import ItemsList from './components/ItemsList';
 
 function App() {
   return (
     <div className="App">
+    <h1 className='wepageHeading'>WestElm Products</h1>
       <ItemsList />
     </div>
   );
