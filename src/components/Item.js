423f27f7267a6600dba474c97ace077b72fdd3d7
Deepali2 <hdgarg@gmail.com>
2019-06-16 11:40:15 -0700

    renders the overlay as a carousel

--- src/components/Item.js
+++ src/components/Item.js
@@ -56,23 +56,24 @@ import Carousel from './Carousel';
 import get from 'lodash.get'; 
 
 function Item(props) {
+  const {item} = props;
+  var thumbnailImagesUrls = item.images.map(image => image.href);
   const [overlayShow, setOverlayShow] = useState();
   const[heroImageUrl, setHeroImageUrl] = useState(
     get(props, 'item.hero.href', 'hero') //object, path, default value
   );  
-  const [imgUrls, setImgUrls] = useState([]);
-  const {item} = props;
-  var thumbnailImagesUrls = item.images.map(image => image.href);
+  const [imgUrls, setImgUrls] = useState([heroImageUrl, ...thumbnailImagesUrls]); 
+  
 
   const {low:lowSellingPrice, high: highSellingPrice} = get(
     item,
     'priceRange.selling'
   );
-  setImgUrls(heroImageUrl, ...thumbnailImagesUrls);
+  
   
   const clickHandler = (e)=> { 
     // e.target.classList.add("selectedThumbnail");
-    setHeroImageUrl(e.target.src);
+    setHeroImageUrl(e.target.src);    
   }; 
   
   const handleOverlayShow = () => {
@@ -90,13 +91,13 @@ function Item(props) {
           <h1 className="item-name">{item.name}</h1>
           <p className="item-price">${lowSellingPrice} - ${highSellingPrice}</p>          
         </div> 
-        <div><img src={heroImageUrl} alt='hero' className='heroImage' onClick={this.handleOverlayShow} /></div>
+        <div><img src={heroImageUrl} alt='hero' className='heroImage' onClick={handleOverlayShow} /></div>
         
         <div className='thumbnails'>            
           <ul className="all-Thumbnail-Images">
-            <li><img src={item.thumbnail.href} alt='thumbnail' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>
+            <li><img src={item.thumbnail.href} alt='thumbnail' className='thumbnail' onClick={(e) => clickHandler(e)} /></li>
             {item.images.map((image, index) => (
-            <li key={index}><img src={image.href} alt='tiny' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>            
+            <li key={index}><img src={image.href} alt='tiny' className='thumbnail' onClick={(e) => clickHandler(e)} /></li>            
             ))}
           </ul>
         </div> 
