e38bf273d3db18423a7cd623fa6ba8e95e169506
Deepali2 <hdgarg@gmail.com>
2019-06-15 23:03:59 -0700

    places the imgUrls state back on the Item Component

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -6,19 +6,13 @@ class ItemsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      overlayShow: false,
-      imgUrls: 
-        [
-        'https://www.westelm.com/weimgs/ab/images/wcm/products/201919/0016/arell-rug-2-m.jpg',
-        'https://www.westelm.com/weimgs/ab/images/wcm/products/201919/0016/arell-rug-1-m.jpg'
-        ]     
+      overlayShow: false        
     }
     this.handleOverlayShow = this.handleOverlayShow.bind(this);
     this.resetOverlayShows = this.resetOverlayShows.bind(this);
   }        
   
   handleOverlayShow(e) {
-    console.log(e.target)
     this.setState({overlayShow: true});
   }
 
