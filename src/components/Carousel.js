77148b74551415443693147d73672c74cf8aa148
Deepali2 <hdgarg@gmail.com>
2019-06-16 08:36:47 -0700

    refactors the Carousel Component using React Hooks

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,49 +1,25 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import ImageSlide from './ImageSlide';
 import Arrow from './Arrow';
 
-class Carousel extends Component {
-  constructor (props) {
-  super(props);		
-  this.state = {
-  currentImageIndex: 0  
+const Carousel = props => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+   
+  const moveSlide = newIndex => {
+    if (newIndex >= imgUrls.length) newIndex = 0;
+    if (newIndex < 0) newIndex = imgUrls.length;
+    setCurrentImageIndex(newIndex);
   };
 
-  this.nextSlide = this.nextSlide.bind(this);
-  this.previousSlide = this.previousSlide.bind(this);
-}
-
-  previousSlide () {
-    const lastIndex = this.props.imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === 0;
-    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
-    
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  nextSlide () {
-    const lastIndex = this.props.imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === lastIndex;
-    const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
-
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  render () {
-    return (          
-      <div className="carousel">
-        <Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
-        <ImageSlide url={ this.props.imgUrls[this.state.currentImageIndex] } />
-        <Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
-      </div>
-    );
-  }
+  const {imgUrls} = props;
+  
+  return (          
+    <div className="carousel">
+      <Arrow direction="left" clickFunction={() => moveSlide(currentImageIndex - 1)} glyph="&#9664;" />
+      <ImageSlide url={imgUrls[currentImageIndex]} />
+      <Arrow direction="right" clickFunction={() => moveSlide(currentImageIndex + 1)} glyph="&#9654;" />
+    </div>
+  );  
 }
 
 export default Carousel;
