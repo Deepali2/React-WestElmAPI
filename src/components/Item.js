efefa81aa6231bb0fdd4090bd393bd01210fce2a
Deepali2 <hdgarg@gmail.com>
2019-06-16 11:13:51 -0700

    refactors using React Hooks

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,54 +1,110 @@
-import React, {Component} from "react";
-import Carousel from './Carousel'
+// import React, {Component} from "react";
+// import Carousel from './Carousel'
 
-class Item extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      imgUrls: [],
-      imageUrl: props.item.hero.href,
-    }
-    this.clickHandler = this.clickHandler.bind(this);     
-  }  
+// class Item extends Component{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       imgUrls: [],
+//       imageUrl: props.item.hero.href,
+//     }
+//     this.clickHandler = this.clickHandler.bind(this);     
+//   }  
 
-  componentDidMount(props) {
-    let item = this.props.item;
-    let heroImageUrl = item.hero.href;
-    let thumbnailImagesUrls = item.images.map(image => image.href);
-    let imgUrls = [heroImageUrl, ...thumbnailImagesUrls];
-    this.setState({imgUrls});
-  }
+//   componentDidMount(props) {
+//     let item = this.props.item;
+//     let heroImageUrl = item.hero.href;
+//     let thumbnailImagesUrls = item.images.map(image => image.href);
+//     let imgUrls = [heroImageUrl, ...thumbnailImagesUrls];
+//     this.setState({imgUrls});
+//   }
   
-  clickHandler(e) { 
-    // e.target.classList.add("selectedThumbnail");
-    this.setState({imageUrl: e.target.src});
-  }
+//   clickHandler(e) { 
+//     // e.target.classList.add("selectedThumbnail");
+//     this.setState({imageUrl: e.target.src});
+//   }
 
-  render() {
-    let item = this.props.item;
-    return (
-      <li className="card">           
-        {this.props.overlayShow ? <Carousel imgUrls={this.state.imgUrls} resetOverlayShows={this.props.resetOverlayShows} /> : null}
-        <div>
-          <div className='name-price'>
-            <h1 className="item-name">{item.name}</h1>
-            <p className="item-price">${item.priceRange.selling.low} - ${item.priceRange.selling.high}</p>          
-          </div> 
-          <div><img src={this.state.imageUrl} alt='hero' className='heroImage' onClick={this.props.handleOverlayShow} /></div>
+//   render() {
+//     let item = this.props.item;
+//     return (
+//       <li className="card">           
+//         {this.props.overlayShow ? <Carousel imgUrls={this.state.imgUrls} resetOverlayShows={this.props.resetOverlayShows} /> : null}
+//         <div>
+//           <div className='name-price'>
+//             <h1 className="item-name">{item.name}</h1>
+//             <p className="item-price">${item.priceRange.selling.low} - ${item.priceRange.selling.high}</p>          
+//           </div> 
+//           <div><img src={this.state.imageUrl} alt='hero' className='heroImage' onClick={this.props.handleOverlayShow} /></div>
           
-          <div className='thumbnails'>            
-            <ul className="all-Thumbnail-Images">
-              <li><img src={item.thumbnail.href} alt='thumbnail' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>
-              {item.images.map((image, index) => (
-              <li key={index}><img src={image.href} alt='tiny' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>            
-              ))}
-            </ul>
-          </div> 
-        </div>  
-      </li>
+//           <div className='thumbnails'>            
+//             <ul className="all-Thumbnail-Images">
+//               <li><img src={item.thumbnail.href} alt='thumbnail' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>
+//               {item.images.map((image, index) => (
+//               <li key={index}><img src={image.href} alt='tiny' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>            
+//               ))}
+//             </ul>
+//           </div> 
+//         </div>  
+//       </li>
+     
+//     )
+//   }  
+// }
+
+import React, {useState} from "react";
+import Carousel from './Carousel';
+import get from 'lodash.get'; 
+
+function Item(props) {
+  const [overlayShow, setOverlayShow] = useState();
+  const[heroImageUrl, setHeroImageUrl] = useState(
+    get(props, 'item.hero.href', 'hero') //object, path, default value
+  );  
+  const [imgUrls, setImgUrls] = useState([]);
+  const {item} = props;
+  var thumbnailImagesUrls = item.images.map(image => image.href);
+
+  const {low:lowSellingPrice, high: highSellingPrice} = get(
+    item,
+    'priceRange.selling'
+  );
+  setImgUrls(heroImageUrl, ...thumbnailImagesUrls);
+  
+  const clickHandler = (e)=> { 
+    // e.target.classList.add("selectedThumbnail");
+    setHeroImageUrl(e.target.src);
+  }; 
+  
+  const handleOverlayShow = () => {
+    setOverlayShow(true)
+  }
+    
+  return (
+    <li className="card" onClick={() => setOverlayShow(true)}>           
+      {overlayShow && (
+        <Carousel imgUrls={imgUrls} resetOverlayShows={() => setOverlayShow(false)}/>
+      )}
+      {/* {this.props.overlayShow ? <Carousel imgUrls={this.state.imgUrls} resetOverlayShows={this.props.resetOverlayShows} /> : null} */}
+      <div>
+        <div className='name-price'>
+          <h1 className="item-name">{item.name}</h1>
+          <p className="item-price">${lowSellingPrice} - ${highSellingPrice}</p>          
+        </div> 
+        <div><img src={heroImageUrl} alt='hero' className='heroImage' onClick={this.handleOverlayShow} /></div>
+        
+        <div className='thumbnails'>            
+          <ul className="all-Thumbnail-Images">
+            <li><img src={item.thumbnail.href} alt='thumbnail' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>
+            {item.images.map((image, index) => (
+            <li key={index}><img src={image.href} alt='tiny' className='thumbnail' onClick={(e) => this.clickHandler(e)} /></li>            
+            ))}
+          </ul>
+        </div> 
+      </div>  
+    </li>
      
     )
-  }  
+    
 }
 
 export default Item;
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,41 +1,56 @@
-import React, {Component} from "react";
+import React from "react";
 import Item from './Item';
 import data from './data.js';
 
-class ItemsList extends Component {  
-  constructor(props) {
-    super(props);
-    this.state = {
-      overlayShow: false        
-    }
-    this.handleOverlayShow = this.handleOverlayShow.bind(this);
-    this.resetOverlayShows = this.resetOverlayShows.bind(this);
-  }        
+// class ItemsList extends Component {  
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       overlayShow: false        
+//     }
+//     this.handleOverlayShow = this.handleOverlayShow.bind(this);
+//     this.resetOverlayShows = this.resetOverlayShows.bind(this);
+//   }        
   
-  handleOverlayShow(e) {
-    this.setState({overlayShow: true});
-  }
+//   handleOverlayShow(e) {
+//     this.setState({overlayShow: true});
+//   }
 
-  resetOverlayShows() {
-    this.setState({overlayShow: false});
-  }
+//   resetOverlayShows() {
+//     this.setState({overlayShow: false});
+//   }
 
-  render() {
-    return (
-      <div >
-        <ul className='container'>
-        {data.groups.map(item => 
-        (<Item 
-          item={item} 
-          key={item.id} 
-          overlayShow={this.state.overlayShow} 
-          handleOverlayShow = {this.handleOverlayShow}
-          imgUrls = {this.state.imgUrls}
-          />))}
-        </ul>
-      </div>
-    ); 
-  }            
-}
+//   render() {
+//     return (
+//       <div >
+//         <ul className='container'>
+//         {data.groups.map(item => 
+//         (<Item 
+//           item={item} 
+//           key={item.id} 
+//           overlayShow={this.state.overlayShow} 
+//           handleOverlayShow = {this.handleOverlayShow}
+//           imgUrls = {this.state.imgUrls}
+//           />))}
+//         </ul>
+//       </div>
+//     ); 
+//   }            
+// }
+
+const ItemsList = (props) =>  {   
+  return (
+    <div >
+      <ul className='container'>
+      {data.groups.map(item => 
+      (<Item 
+        item={item} 
+        key={item.id}         
+        />))}
+      </ul>
+    </div>
+  ); 
+}            
+
 
 export default ItemsList;
