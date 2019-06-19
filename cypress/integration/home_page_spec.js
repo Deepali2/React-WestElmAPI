7c78f799625680389dab13edb980c095c3fc3fce
Deepali2 <hdgarg@gmail.com>
2019-06-18 22:43:26 -0700

    adds a few more tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,27 +1,32 @@
 /// <reference types="Cypress" />
 
 describe('FrontEnd', function() {
+  beforeEach(() => {
+    cy.visit('/')
+  })
   it('successfully loads', function() {
     cy.visit('/')
   })
-})
-
-describe('Express Server', function() {
-  it('successfully loads', function() {
-    cy.request('http://localhost:3001')
-  })
-})
-
-describe('renders the list of items', () => {
   it('renders the list of items', () => {
     cy.visit('/')
     cy.get(".container")
   })
-})
-
-describe('renders each item', () => {
   it ('renders each item', () => {
     cy.visit('/')
     cy.get('.card')
   })
+  it('renders when a big image is clicked on', () => {
+    cy.visit('/')
+    cy.get('.carousel')
+  })
 })
+
+describe('Express Server', function() {
+  it('successfully starts', function() {
+    cy.request('http://localhost:3001')
+  })
+})
+
+
+
+
